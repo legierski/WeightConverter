0b2c8f59256fa25108b0254e61fa4e108f61ce82
Peter Legierski <peter@gathercontent.com>
2012-06-28 01:07:36 +0100

    First commit

---
+++ js/calculate.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+	
+	source = $('.ajax-source');
+	target = $('.ajax-target');
+
+	$('.ajax-convert-trigger').click(function() {
+		
+		var option = $('.ajax-radio.active').attr('data-radio-value');
+
+		if(!isNaN(source.val())) {
+			if(option == '1') {
+				var weight = Math.round(source.val()*6.35029318*100)/100;
+				target.val(weight+' kg');
+			}
+			else if(option == '2') {
+				var weight = Math.round(source.val()*0.157473044*100)/100;
+				target.val(weight+' stones');
+			}	
+		}
+		return false;
+	});
+
+	$('.ajax-radio').click(function() {
+
+		$('.ajax-radio').removeClass('active');
+		$(this).addClass('active');
+
+		source.val('');
+		target.val('');
+
+		var option = $(this).attr('data-radio-value');
+
+		if(option == '1') {
+			source.attr('placeholder', 'Weight in stones...');
+			target.attr('placeholder', 'Weight in kg...');
+		}
+		else {
+			source.attr('placeholder', 'Weight in kg...');
+			target.attr('placeholder', 'Weight in stones...');
+		}
+	});
+});
